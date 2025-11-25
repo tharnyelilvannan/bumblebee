7da038007b4421ccd103f6f3bf11eaa3ca73621a
tharnyelilvannan <tharny2005@outlook.com>
2025-11-25 13:42:59 -0500

    Update documentation

--- basic/basic.ino
+++ basic/basic.ino
@@ -1,61 +1,67 @@
 int ECHO = 8; // echo value back 
 int TRIG = 9; // triggers sound output
-int duration; 
-float distance;
-float meter;
-int LED = 13;
-int IR = 7; // input pin for IR sensor
-int isObstacle_IR = HIGH; // for IR sensor, high means yes
+int duration; // duration it takes for pulse echo off object and come back
+float distance; // distance to object
+int IR_LED = 13; // LED on IR sensor
+int IR_INPUT = 7; // input pin for IR sensor
+int IR_isObstacle = HIGH; // for IR sensor, high means yes
 
 void setup() {
+
   Serial.begin(9600);
 
-  pinMode(LED, OUTPUT);
+  // initialize IR
   pinMode(IR, INPUT);
+  pinMode(LED, OUTPUT);
+
+  // initialize US
   pinMode(TRIG, OUTPUT);
   pinMode(ECHO, INPUT);
 
+  // ensure trigger is set to low at first
   digitalWrite(TRIG, LOW);
-
 
 }
 
 void loop() {
 
   // IR
-  isObstacle_IR = digitalRead(IR);
-  if (isObstacle_IR == LOW)
+  IR_isObstacle = digitalRead(IR_INPUT);
+
+  if (IR_isObstacle == LOW)
   {
-    Serial.println("OBSTACLE!!, OBSTACLE!!");
-    digitalWrite(LED, HIGH);
+    Serial.println("White surface.");
+    digitalWrite(IR_LED, HIGH);
   }
   else
   {
-    Serial.println("clear");
-    digitalWrite(LED, LOW);
+    Serial.println("Black surface.");
+    digitalWrite(IR_LED, LOW);
   }
+
   delay(200);
 
-  // ULTRASONIC
+  // US
    
-  digitalWrite(TRIG, HIGH); 
+  digitalWrite(TRIG, HIGH); // pulse trigger
   delayMicroseconds(10); 
-  digitalWrite(TRIG, LOW); 
-  duration = pulseIn(ECHO, HIGH);
-  if(duration>=38000){
-    Serial.print("Out range"); 
+  digitalWrite(TRIG, LOW);
+  duration = pulseIn(ECHO, HIGH); // read echo
+
+  if (duration >= 38000) {
+
+    Serial.print("Out of range"); 
+
   }
   else
   {
-    distance = duration/58; 
+    // calculate distance to object
+    distance = duration / 58; 
     Serial.print(distance); 
-    Serial.print("cm");
-    meter=distance/100;
-    Serial.print("\t");
-    Serial.print(meter);
-    Serial.println("m");
-  }
-  delay(1000); 
+    Serial.println(" cm");
 
+  }
+
+  delay(1000); 
 
 }
